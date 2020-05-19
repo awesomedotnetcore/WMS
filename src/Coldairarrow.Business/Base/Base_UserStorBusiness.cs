3c14f4327b16fec6afbb3d2a36e23114235cfa80
刘巨 <liuju150@outlook.com>
2020-05-19 16:19:40 +0800

    仓库权限初始化

---
+++ src/Coldairarrow.Business/Base/Base_UserStorBusiness.cs
@@ -0,0 +1,65 @@
+﻿using Coldairarrow.Entity.Base;
+using Coldairarrow.Util;
+using EFCore.Sharding;
+using LinqKit;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+
+namespace Coldairarrow.Business.Base
+{
+    public class Base_UserStorBusiness : BaseBusiness<Base_UserStor>, IBase_UserStorBusiness, ITransientDependency
+    {
+        public Base_UserStorBusiness(IRepository repository)
+            : base(repository)
+        {
+        }
+
+        #region 外部接口
+
+        public async Task<PageResult<Base_UserStor>> GetDataListAsync(PageInput<ConditionDTO> input)
+        {
+            var q = GetIQueryable();
+            var where = LinqHelper.True<Base_UserStor>();
+            var search = input.Search;
+
+            //筛选
+            if (!search.Condition.IsNullOrEmpty() && !search.Keyword.IsNullOrEmpty())
+            {
+                var newWhere = DynamicExpressionParser.ParseLambda<Base_UserStor, bool>(
+                    ParsingConfig.Default, false, $@"{search.Condition}.Contains(@0)", search.Keyword);
+                where = where.And(newWhere);
+            }
+
+            return await q.Where(where).GetPageResultAsync(input);
+        }
+
+        public async Task<Base_UserStor> GetTheDataAsync(string id)
+        {
+            return await GetEntityAsync(id);
+        }
+
+        public async Task AddDataAsync(Base_UserStor data)
+        {
+            await InsertAsync(data);
+        }
+
+        public async Task UpdateDataAsync(Base_UserStor data)
+        {
+            await UpdateAsync(data);
+        }
+
+        public async Task DeleteDataAsync(List<string> ids)
+        {
+            await DeleteAsync(ids);
+        }
+
+        #endregion
+
+        #region 私有成员
+
+        #endregion
+    }
+}
---
+++ src/Coldairarrow.IBusiness/Base/IBase_UserStorBusiness.cs
@@ -0,0 +1,16 @@
+﻿using Coldairarrow.Entity.Base;
+using Coldairarrow.Util;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Coldairarrow.Business.Base
+{
+    public interface IBase_UserStorBusiness
+    {
+        Task<PageResult<Base_UserStor>> GetDataListAsync(PageInput<ConditionDTO> input);
+        Task<Base_UserStor> GetTheDataAsync(string id);
+        Task AddDataAsync(Base_UserStor data);
+        Task UpdateDataAsync(Base_UserStor data);
+        Task DeleteDataAsync(List<string> ids);
+    }
+}
