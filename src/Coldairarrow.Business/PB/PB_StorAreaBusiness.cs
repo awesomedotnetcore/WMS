c9ffbe6f51f7ce503ab8ededa14230b5b332f5bf
彭庆 <pengqing@zeqp.net>
2020-05-21 15:31:41 +0800

    创建货区管理

---
+++ src/Coldairarrow.Business/PB/PB_StorAreaBusiness.cs
@@ -0,0 +1,65 @@
+﻿using Coldairarrow.Entity.PB;
+using Coldairarrow.Util;
+using EFCore.Sharding;
+using LinqKit;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+
+namespace Coldairarrow.Business.PB
+{
+    public class PB_StorAreaBusiness : BaseBusiness<PB_StorArea>, IPB_StorAreaBusiness, ITransientDependency
+    {
+        public PB_StorAreaBusiness(IRepository repository)
+            : base(repository)
+        {
+        }
+
+        #region 外部接口
+
+        public async Task<PageResult<PB_StorArea>> GetDataListAsync(PageInput<ConditionDTO> input)
+        {
+            var q = GetIQueryable();
+            var where = LinqHelper.True<PB_StorArea>();
+            var search = input.Search;
+
+            //筛选
+            if (!search.Condition.IsNullOrEmpty() && !search.Keyword.IsNullOrEmpty())
+            {
+                var newWhere = DynamicExpressionParser.ParseLambda<PB_StorArea, bool>(
+                    ParsingConfig.Default, false, $@"{search.Condition}.Contains(@0)", search.Keyword);
+                where = where.And(newWhere);
+            }
+
+            return await q.Where(where).GetPageResultAsync(input);
+        }
+
+        public async Task<PB_StorArea> GetTheDataAsync(string id)
+        {
+            return await GetEntityAsync(id);
+        }
+
+        public async Task AddDataAsync(PB_StorArea data)
+        {
+            await InsertAsync(data);
+        }
+
+        public async Task UpdateDataAsync(PB_StorArea data)
+        {
+            await UpdateAsync(data);
+        }
+
+        public async Task DeleteDataAsync(List<string> ids)
+        {
+            await DeleteAsync(ids);
+        }
+
+        #endregion
+
+        #region 私有成员
+
+        #endregion
+    }
+}
