e912ce145d4c3cb7a3405ff597f8bc681c3677bd
刘巨 <liuju150@outlook.com>
2020-07-03 11:11:08 +0800

    货品统计与预警功能

---
+++ src/Coldairarrow.Business/Report/Report_MaterialSummaryBussiness.cs
@@ -0,0 +1,63 @@
+﻿using Coldairarrow.Entity.IT;
+using Coldairarrow.Entity.PB;
+using Coldairarrow.Entity.TD;
+using Coldairarrow.IBusiness.Report;
+using Coldairarrow.Util;
+using EFCore.Sharding;
+using LinqKit;
+using Microsoft.EntityFrameworkCore;
+using OracleInternal.SqlAndPlsqlParser.LocalParsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Coldairarrow.Business.IT
+{
+    public partial class Report_MaterialSummaryBussiness : BaseBusiness<IT_LocalMaterial>, IReport_MaterialSummaryBussiness, ITransientDependency
+    {
+        readonly IServiceProvider _ServiceProvider;
+        public Report_MaterialSummaryBussiness(IDbAccessor db, IServiceProvider svc)
+            : base(db)
+        {
+            _ServiceProvider = svc;
+        }
+        public async Task<PageResult<Report_MaterialSummaryVM>> GetDataListAsync(PageInput<Report_MaterialSummaryQM> input)
+        {
+            var search = input.Search;
+            var mQuery = from lm in Db.GetIQueryable<IT_LocalMaterial>()
+                         group lm by lm.MaterialId into g
+                         select new { MaterialId = g.Key, SumCount = g.Sum(p => p.Num) } into lms
+                         join m in Db.GetIQueryable<PB_Material>() on lms.MaterialId equals m.Id
+                         join mt in Db.GetIQueryable<PB_MaterialType>() on m.MaterialTypeId equals mt.Id
+                         join me in Db.GetIQueryable<PB_Measure>() on m.MeasureId equals me.Id
+                         select new Report_MaterialSummaryVM
+                         {
+                             Id = m.Id,
+                             Name = m.Name,
+                             Code = m.Code,
+                             BarCode = m.BarCode,
+                             SimpleName = m.SimpleName,
+                             MaterialTypeId = m.MaterialTypeId,
+                             MaterialTypeName = mt.Name,
+                             MeasureName = me.Name,
+                             Spec = m.Spec,
+                             Max = m.Max,
+                             Min = m.Min,
+                             Price = m.Price,
+                             SumCount = lms.SumCount
+                         };
+            if (!search.MaterialTypeId.IsNullOrEmpty())
+                mQuery = mQuery.Where(w => w.MaterialTypeId == search.MaterialTypeId);
+            if (!search.MaterialName.IsNullOrEmpty())
+                mQuery = mQuery.Where(w => w.Name.Contains(search.MaterialName) || w.Code.Contains(search.MaterialName) || w.SimpleName.Contains(search.MaterialName) || w.BarCode.Contains(search.MaterialName));
+            if (search.MinAlert)
+                mQuery = mQuery.Where(w => w.Min >= w.SumCount && w.Min.HasValue);
+            if (search.MaxAlert)
+                mQuery = mQuery.Where(w => w.Max <= w.SumCount && w.Max.HasValue);
+            return await mQuery.GetPageResultAsync(input);
+        }
+    }
+}
---
+++ src/Coldairarrow.IBusiness/Report/IReport_MaterialSummaryBussiness.cs
@@ -0,0 +1,81 @@
+﻿using Coldairarrow.Entity.PB;
+using Coldairarrow.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coldairarrow.IBusiness.Report
+{
+    /// <summary>
+    /// 货品统计
+    /// </summary>
+    public interface IReport_MaterialSummaryBussiness
+    {
+        Task<PageResult<Report_MaterialSummaryVM>> GetDataListAsync(PageInput<Report_MaterialSummaryQM> input);
+    }
+    public class Report_MaterialSummaryQM
+    {
+        public string MaterialTypeId { get; set; }
+        public string MaterialName { get; set; }
+        public bool MinAlert { get; set; }
+        public bool MaxAlert { get; set; }
+    }
+    public class Report_MaterialSummaryVM
+    {
+        public String Id { get; set; }
+
+        /// <summary>
+        /// 物料名称
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// 物料编码
+        /// </summary>
+        public String Code { get; set; }
+
+        /// <summary>
+        /// 条码
+        /// </summary>
+        public String BarCode { get; set; }
+
+        /// <summary>
+        /// 物料简称
+        /// </summary>
+        public String SimpleName { get; set; }
+
+        public string MaterialTypeId { get; set; }
+        /// <summary>
+        /// 物料类型ID
+        /// </summary>
+        public String MaterialTypeName { get; set; }
+
+        /// <summary>
+        /// 单位ID
+        /// </summary>
+        public String MeasureName { get; set; }
+
+        /// <summary>
+        /// 物料规格
+        /// </summary>
+        public String Spec { get; set; }
+
+        /// <summary>
+        /// 上限数量
+        /// </summary>
+        public Double? Max { get; set; }
+
+        /// <summary>
+        /// 下限数量
+        /// </summary>
+        public Double? Min { get; set; }
+
+        /// <summary>
+        /// 单价
+        /// </summary>
+        public Double? Price { get; set; }
+
+        public double SumCount { get; set; }
+    }
+}
