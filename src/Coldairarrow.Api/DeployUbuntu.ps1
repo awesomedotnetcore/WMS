30ec38bca733132da6b812dd4077ce31512f1017
刘巨 <liuju150@outlook.com>
2020-08-25 14:18:57 +0800

    Docker

--- src/Coldairarrow.Api/DeployUbuntu.ps1
+++ src/Coldairarrow.Api/DeployUbuntu.ps1
@@ -7,7 +7,7 @@ Write-Host 'Build Completed' -ForegroundColor Green
 
 Write-Host 'Compress Starting' -ForegroundColor Yellow
 $CurDateString=Get-Date -Format "yyyyMMddHHmmss"
-$ZIPFileName="WMSAPI"+$CurDateString+".zip"
+$ZIPFileName="Deploy"+$CurDateString+".zip"
 $ZIPFilePath=$CurPath+"\"+$ZIPFileName
 $CompressPath=$OutputPath+"*"
 Compress-Archive -Path $CompressPath -DestinationPath $ZIPFilePath
@@ -18,28 +18,31 @@ Write-Host 'Deploy Starting' -ForegroundColor Yellow
 # $Password = Read-Host -Prompt "Please enter the server password" -AsSecureString
 # Write-Host 'Start connecting to the server' -ForegroundColor Yellow
 # $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $Password
-$Session = New-PSSession -HostName 10.76.20.51 -UserName giant -KeyFilePath "C:\Users\Administrator\id_rsa"
+$Session = New-PSSession -HostName 10.76.20.162 -UserName root -KeyFilePath "C:\Users\Administrator\.ssh\id_rsa"
 $Session
 Write-Host 'Successfully connected to the server' -ForegroundColor Green
 Write-Host 'Start copying files to the server' -ForegroundColor Yellow
-$RemotePath="/home/giant/ZEQPWMS/"
+$RemotePath="/srv/Deploy/"
 Copy-Item $ZIPFilePath -Destination $RemotePath -ToSession $Session
 Write-Host 'Copy files completed' -ForegroundColor Green
 Write-Host 'Start Expand files on the server' -ForegroundColor Yellow
-$RemoteDestinationPath=$RemotePath+"WMSAPI/"
+$RemoteDestinationPath=$RemotePath+"API/"
 $RemoteZipPath=$RemotePath+$ZIPFileName
-# Invoke-Command -Session $Session -ScriptBlock {Stop-Service -Name "W3SVC"}
-#Invoke-Command -Session $Session -ScriptBlock {iisreset}
-Invoke-Command -Session $Session -ScriptBlock {param($p) chmod 777 $p -R} -ArgumentList $RemotePath
-
 Invoke-Command -Session $Session -ScriptBlock {param($p) Remove-Item -Path $p -Recurse -Force} -ArgumentList $RemoteDestinationPath
 Invoke-Command -Session $Session -ScriptBlock {param($p,$dp) Expand-Archive -Path $p -DestinationPath $dp} -ArgumentList $RemoteZipPath,$RemoteDestinationPath
 
-Invoke-Command -Session $Session -ScriptBlock {param($p) chmod 777 $p -R} -ArgumentList $RemotePath
 $ConfigProductionFile=$RemoteDestinationPath+"appsettings.Production.json"
 Invoke-Command -Session $Session -ScriptBlock {param($p) Remove-Item -Path $p -Force} -ArgumentList $ConfigProductionFile
-# Invoke-Command -Session $Session -ScriptBlock {Start-Service -Name "W3SVC"}
 Write-Host 'Expand Completed' -ForegroundColor Green
+
+Write-Host 'Deploy to Docker Starting' -ForegroundColor Yellow
+Invoke-Command -Session $Session -ScriptBlock {docker stop api}
+Invoke-Command -Session $Session -ScriptBlock {docker rm api}
+Invoke-Command -Session $Session -ScriptBlock {docker rmi api}
+Invoke-Command -Session $Session -ScriptBlock {docker build -t api /srv/Deploy/API}
+Invoke-Command -Session $Session -ScriptBlock {docker run -d -p 5000:5000 --name api api}
+Write-Host 'Deploy to Docker Completed' -ForegroundColor Green
+
 Remove-Item -Path $ZIPFilePath
-Write-Host 'Disconnected from server' -ForegroundColor Yellow
 Write-Host 'Deploy Completed' -ForegroundColor Green
+
